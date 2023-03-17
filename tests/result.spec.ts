e800adbeb2f5fc6c73e6150fdf3bdff06ed3c169
Andrew X. Shah <drewshah0@gmail.com>
2023-03-17 01:26:46 -0600

    test: add match tests

--- tests/result.spec.ts
+++ tests/result.spec.ts
@@ -1,4 +1,4 @@
-import {Ok, Err, Result} from "../src";
+import {Ok, Err, Result, match, Some, None} from "../src";
 
 describe("Result", () => {
     describe("Ok", () => {
@@ -121,6 +121,63 @@ describe("Result", () => {
                 expect(() =>
                     result.expect("Custom error message"),
                 ).toThrowError("Custom error message");
+            });
+        });
+    });
+
+    describe("Match", () => {
+        describe("standalone", () => {
+            it("should match an Ok value to a pattern", () => {
+                const val = Ok(42);
+                const res = match(val, {
+                    ok: v => String(v),
+                    err: _ => "Enexpected error",
+                });
+                expect(res).toBe("42");
+            });
+            it("should match an Err value to a pattern", () => {
+                const val = Err("Something went wrong");
+                const res = match(val, {
+                    ok: v => String(v),
+                    err: _ => "Enexpected error",
+                });
+                expect(res).toBe("Enexpected error");
+            });
+        });
+        describe("Result.match", () => {
+            it("should match an Ok value to a pattern", () => {
+                const val = Ok(42);
+                const res = val.match({
+                    ok: v => String(v),
+                    err: _ => "Enexpected error",
+                });
+                expect(res).toBe("42");
+            });
+            it("should match an Err value to a pattern", () => {
+                const val = Err("Something went wrong");
+                const res = val.match({
+                    ok: v => String(v),
+                    err: _ => "Enexpected error",
+                });
+                expect(res).toBe("Enexpected error");
+            });
+        });
+        describe("Option.match", () => {
+            it("should match a Some value to a pattern", () => {
+                const val = Some(42);
+                const res = val.match({
+                    some: v => String(v),
+                    none: () => "Enexpected error",
+                });
+                expect(res).toBe("42");
+            });
+            it("should match a None value to a pattern", () => {
+                const val = None();
+                const res = val.match({
+                    some: v => String(v),
+                    none: () => "Enexpected error",
+                });
+                expect(res).toBe("Enexpected error");
             });
         });
     });
