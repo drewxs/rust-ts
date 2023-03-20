480e3fc1f91d5044b079dbc56fc16f2d71cefa5f
Andrew X. Shah <drewshah0@gmail.com>
2023-03-19 22:07:23 -0600

    docs(match): include docs for declarations only

--- src/lib/match.ts
+++ src/lib/match.ts
@@ -2,11 +2,11 @@ import {Option, OptionPattern} from "./option";
 import {is_result, Result, ResultPattern} from "./result";
 
 /**
- * Function overload for `match` on a `Result`.
+ * Pattern matches over `Result` variants.
  *
- * @typeParam T - The type of the successful result value.
+ * @typeParam T - The type of the value contained in the `Result`.
  * @typeParam E - The type of the error value.
- * @typeParam R - The return type of the pattern.
+ * @typeParam R - The type of the result of the match pattern.
  * @param input - The `Result` to match on.
  * @param pattern - The pattern to match over.
  * @returns the result of the pattern match.
@@ -17,26 +17,16 @@ export function match<T, E, R>(
 ): R;
 
 /**
- * Function overload for `match` on a `Option`.
+ * Pattern matches over `Option` variants.
  *
  * @typeParam T - The type of the value contained in the `Option`.
- * @typeParam R - The return type of the pattern.
- * @param input - The `Result` to match on.
+ * @typeParam R - The type of the result of the match pattern.
+ * @param input - The `Option` to match on.
  * @param pattern - The pattern to match over.
  * @returns the result of the pattern match.
  */
 export function match<T, R>(input: Option<T>, pattern: OptionPattern<T, R>): R;
 
-/**
- * Pattern matches over variants of `Result` or `Option`.
- *
- * @typeParam T - The type of the value contained in the `Result` or `Option`.
- * @typeParam E - The type of the error value.
- * @typeParam R - The type of the result of the match pattern.
- * @param input - The `Result` or `Option` to match on.
- * @param pattern - The pattern to match on.
- * @returns the result of the pattern match.
- */
 export function match<T, E, R>(
     input: Result<T, E> | Option<T>,
     pattern: ResultPattern<T, E, R> | OptionPattern<T, R>,
