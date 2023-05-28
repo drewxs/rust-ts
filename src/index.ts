151e529a7b48357119fedb11f9524ca20b129142
Andrew X. Shah <drewshah0@gmail.com>
2023-05-27 19:15:46 -0600

    fix(lib): missing fetchx export

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import {Some as _Some, None as _None, Option} from "./lib/option";
 import {Ok as _Ok, Err as _Err, Result} from "./lib/result";
 import {match} from "./lib/match";
 import {if_let} from "./lib/if_let";
-import {fetchr} from "./lib/fetch";
+import {fetchr, fetchx} from "./lib/fetch";
 
 const Some = <T>(value: T): Option<T> => new _Some(value);
 const None = <T>(): Option<T> => new _None();
 const Ok = <T, E>(value: T): Result<T, E> => new _Ok(value);
 const Err = <T, E>(error: E): Result<T, E> => new _Err(error);
 
-export {Some, None, Option, Ok, Err, Result, match, if_let, fetchr};
+export {Some, None, Option, Ok, Err, Result, match, if_let, fetchr, fetchx};
